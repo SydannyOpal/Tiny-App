be3a546cda1c79df6a36f823456cebd41c65c844
= <sydanny15@gmail.com>
2022-05-18 19:16:19 -0400

    cookies in express

--- express_server.js
+++ express_server.js
@@ -16,8 +16,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 app.set("view engine", "ejs");
 
@@ -34,7 +38,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"],};
   res.render("urls_index", templateVars);
 });
 
@@ -75,6 +79,17 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
